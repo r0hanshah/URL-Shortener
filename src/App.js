b17d17d5df9a12a6a5332ce4e5db1877514f8c19
Rohan Shah <rshah04@gmail.com>
2023-07-16 17:54:29 -0400

    webpage loads, blank square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Form from "./components/Form";
-import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Switch>
-              <Route exact path="/" component={Form} />
-              <Route path="/App" component={Form} />
-            </Switch>
-          </div>
+  return (<Router>
+    <div className="App">
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Routes>
+            <Route exact path='/' component={Form} />
+            <Route path="/app" component={Form} />
+          </Routes>
         </div>
       </div>
-    </Router>
-    // comment
-    // comment
-    // comments
+    </div></Router>
   );
 }
 
